c70431b75564857dab5198908edae8a7b55cc2c7
Leo <yingxiang.huang@swufefic.com>
2019-09-10 23:07:27 +0800

    modified the inherit class of the search engine

--- proxy_pattern/proxy.py
+++ proxy_pattern/proxy.py
@@ -23,7 +23,7 @@ class Logger:
         print('log the call function')
 
 
-class SearchEngine:
+class SearchEngine(metaclass=abc.ABCMeta):
 
     def __init__(self):
         print('init the SearchEngine')
